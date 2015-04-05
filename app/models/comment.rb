37ff43e4808261bcaee5283b71b4611a5530e5f4
Tony G. Bolaño <tonybolanyo@gmail.com>
2015-04-05 22:38:18 +0200

    Agregadas validaciones básicas

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
 
 	belongs_to :post
 
+	validates_presence_of :post_id
+	validates_presence_of :body
+	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
 
 	has_many :comments, dependent: :destroy
+
+	validates_presence_of :title
+	validates_presence_of :body
+	
 end
